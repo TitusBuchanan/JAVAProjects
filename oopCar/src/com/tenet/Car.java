1ec99e2e648742bc6bfc1a88d1765c929ef48842
titus buchanan <titusbuchananjr@gmail.com>
2020-09-01 19:16:41 -0400

    completed car project

--- oopCar/src/com/tenet/Car.java
+++ oopCar/src/com/tenet/Car.java
@@ -1,22 +1,39 @@
 package com.tenet;
 
 public class Car {
-   private int doors;
+   private int doors; //Attributes
     private int wheels;
+    private String color;
+    private String make;
+    private int price;
 
 
 
-    public Car(int doors, int wheels) {
+    public Car(int doors, int wheels, String color, String make, int price) {
         this.doors = doors;
         this.wheels = wheels;
-
+        this.make = make;
+        this.color = color;
+        this.price = price;
     }
 
-    public int getDoors() {
+    public int getDoors() {   //Methods(Getters) function inside of a class
         return doors;
     }
 
     public int getWheels() {
         return wheels;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getPrice(){
+        return price;
     }
 }
--- oopCar/src/com/tenet/Main.java
+++ oopCar/src/com/tenet/Main.java
@@ -3,12 +3,27 @@ package com.tenet;
 public class Main {
 
     public static void main(String[] args) {
-	Car car;
 
-	car = new Car(4,4);
+
+
+	Car car = new Car(2,4,"Yellow","Lambo",350000);
+	Car bugatti = new Car(2,4,"Black","Bugatti",2000000);
+
+
 
 	System.out.println("This Car has "+ car.getWheels() + "WHeels");
 		System.out.println("This Car has "+ car.getDoors() +" Doors");
+		System.out.println("This Car's color is  "+ car.getColor() );
+		System.out.println("This Car's Make is a  "+ car.getMake() );
+
+
+		System.out.println("This Car has "+ bugatti.getWheels() + "WHeels");
+		System.out.println("This Car has "+ bugatti.getDoors() +" Doors");
+		System.out.println("This Car's color is  "+ bugatti.getColor() );
+		System.out.println("This Car's Make is a  "+ bugatti.getMake() );
+
+
+
 
     }
 
