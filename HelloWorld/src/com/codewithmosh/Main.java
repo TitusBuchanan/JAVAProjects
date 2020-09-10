fb2bcff878f5906b02f132ee92e2147f4bc39f28
titus buchanan <titusbuchananjr@gmail.com>
2020-09-10 00:28:54 -0400

    created first Java array and used sort method

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -1,12 +1,16 @@
 package com.codewithmosh;
 
 
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello World" + "!!";
-        System.out.println(message);
+        int[] numbers = {3,2,1,5,4};
+        Arrays.sort(numbers);
+        System.out.println(numbers.length);
+
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
