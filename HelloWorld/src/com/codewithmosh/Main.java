d892f8aa712d2c2851909c5242d011dbc07b5d59
titus buchanan <titusbuchananjr@gmail.com>
2020-09-10 23:26:17 -0400

    added a multi dimensional array

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -6,11 +6,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = {3,2,1,5,4};
-        Arrays.sort(numbers);
-        System.out.println(numbers.length);
-
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.deepToString(numbers));
 
     }
 }
