74fa53e18879b3d20ad22209ad347a828814a798
titus buchanan <titusbuchananjr@gmail.com>
2020-09-11 20:10:05 -0400

    added another variable to multi-dimensional array

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -6,8 +6,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = new int[2][3];
+        int[][] numbers = new int[5][5];
         numbers[0][0] = 1;
+        numbers[4][2] = 2;
         System.out.println(Arrays.deepToString(numbers));
 
     }
