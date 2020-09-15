3a24f4050cb3b23aaba1dcf60abdd8c7e6be4634
titus buchanan <titusbuchananjr@gmail.com>
2020-09-14 20:21:53 -0400

    added another variable

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -10,6 +10,7 @@ public class Main {
         numbers[0][0] = 1;
         numbers[4][2] = 2;
         numbers[3][1] = 3;
+        numbers[0][3] = 4;
         System.out.println(Arrays.deepToString(numbers));
 
     }
