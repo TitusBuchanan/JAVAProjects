b1f450002b3e02e43fc13f8fe099735babdaec02
titus buchanan <titusbuchananjr@gmail.com>
2020-09-17 19:05:47 -0400

    new values added

--- Constants/src/com/tenet/Main.java
+++ Constants/src/com/tenet/Main.java
@@ -3,8 +3,10 @@ package com.tenet;
 public class Main {
 
     public static void main(String[] args) {
-        double result = (double)10 / (double) 3;
-        System.out.println(result);
+        int x = 1;
+        x += 2;
+        System.out.println(x);
+
 
     }
 }
--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -6,12 +6,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = new int[5][5];
-        numbers[0][0] = 1;
-        numbers[4][2] = 2;
-        numbers[3][1] = 3;
-        numbers[0][3] = 4;
-        System.out.println(Arrays.deepToString(numbers));
+        int x = 10 + 3 * 2;
+        System.out.println(x);
 
     }
 }
