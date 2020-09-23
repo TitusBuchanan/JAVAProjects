76410d0823c9d872d2fed0d9cfe013a88ee416d6
titus buchanan <titusbuchananjr@gmail.com>
2020-09-22 21:35:17 -0400

    Yahtzee game

---
+++ CliffYaht/src/com/CliffYahtzee/Main.java
@@ -0,0 +1,32 @@
+package com.CliffYahtzee;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // Random rand = new Random();
+       // Die die = new Die();
+       // System.out.println(die.getValue());
+       // die.roll(rand);
+       // System.out.println(die.getValue());
+       // die.roll(rand);
+       // System.out.println(die.getValue());
+       // die.roll(rand);
+      //  System.out.println(die.getValue());
+      //  die.roll(rand);
+
+        Random rand = new Random();
+        Hand myHand = new Hand();
+        System.out.print(myHand.getDie(0));
+        System.out.print(myHand.getDie(1));
+        System.out.print(myHand.getDie(3));
+        myHand.roll(rand);
+        System.out.print(myHand.getDie(0));
+        System.out.print(myHand.getDie(1));
+        System.out.print(myHand.getDie(3));
+        myHand.roll(rand);
+
+
+    }
+}
---
+++ YahtzeeGame/src/com/Yahtzee/Yahtzee.java
@@ -0,0 +1,6 @@
+package com.Yahtzee;
+
+public class Yahtzee {
+    private int[] dice = new int[5];
+
+}
