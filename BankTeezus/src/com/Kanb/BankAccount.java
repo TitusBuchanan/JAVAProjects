04ff846cb31113a5ab8d8d295f541d0b0e472ae5
titus buchanan <titusbuchananjr@gmail.com>
2020-10-21 19:56:47 -0400

    started new Bank project

---
+++ BankTeezus/src/com/Kanb/BankAccount.java
@@ -0,0 +1,30 @@
+package com.Kanb;
+
+public class BankAccount {
+    private int balance;
+    private String owner;
+    private String accountNum;
+    private String type;
+
+    public BankAccount(int balance, String accountNum, String owner, String type){
+        this.balance = balance;
+        this.owner = owner;
+        this.accountNum = accountNum;
+        this.type = type;
+    }
+
+    public void deposit(int amt) {
+        if( amt > 0) {
+            balance += amt;
+        }
+    }
+
+    public void withdraw(int amt) {
+        if(amt <= amt) {
+            balance -= amt;
+        }
+    }
+
+
+
+}
--- BankTeezus/src/com/Kanb/Main.java
+++ BankTeezus/src/com/Kanb/Main.java
@@ -3,6 +3,6 @@ package com.Kanb;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
