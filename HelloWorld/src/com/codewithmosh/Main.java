ff44de6d4422af87ca2f703032e243ba90149118
titus buchanan <titusbuchananjr@gmail.com>
2020-10-30 01:28:58 -0400

    added examples of logical operators and or orperators

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -8,12 +8,29 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //Or operator
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+
+
+        //Logical Operators
+        //int temperature = 22;
+        //boolean isWarm = temperature > 20 && temperature < 30;
+        //System.out.println(isWarm);
+
+
+
         //comparison operators
+        //int x = 1;
+        //int y = 2;
+        //System.out.println(x <= y);
 
 
         //Reading input
-        Scanner scanner = new Scanner(System.in);
-         System.out.print("Name: ");
+        //Scanner scanner = new Scanner(System.in);
+        // System.out.print("Name: ");
        // String name = scanner.nextLine().trim();
        // System.out.println("You are " + name);
 
