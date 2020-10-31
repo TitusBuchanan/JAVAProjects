77df6adf53df0712ee32133091f9c2e41612237b
titus buchanan <titusbuchananjr@gmail.com>
2020-10-30 21:21:06 -0400

    added a for loop

--- HelloWorld/src/com/codewithmosh/Main.java
+++ HelloWorld/src/com/codewithmosh/Main.java
@@ -8,23 +8,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-      //FizzBuzz
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Number: ");
-        int number = scanner.nextInt();
+     //For Loops
+        for (int i = 0; i < 5; i++){
 
-        if ( number % 5 == 0 ) {
-            if (number % 3 == 0)
-            System.out.println("FizzBuzz");
-            else
-                System.out.println("Fizz");
         }
-        else if( number % 5 == 0)
-            System.out.println("Fizz");
-        else if (number % 3 == 0)
-            System.out.println("Buzz");
-        else
-            System.out.println(number);
+
+
+
+
+
+
+      //FizzBuzz
+       // Scanner scanner = new Scanner(System.in);
+       // System.out.println("Number: ");
+       // int number = scanner.nextInt();
+
+       // if ( number % 5 == 0 ) {
+            //if (number % 3 == 0)
+           // System.out.println("FizzBuzz");
+           // else
+                //System.out.println("Fizz");
+        //}
+       // else if( number % 5 == 0)
+       //     System.out.println("Fizz");
+      //  else if (number % 3 == 0)
+       //     System.out.println("Buzz");
+       // else
+         //   System.out.println(number);
 
 
 
