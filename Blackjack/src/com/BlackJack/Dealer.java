a33e282d93b590b79e4a773b9fa3f3f10de64da8
titus buchanan <titusbuchananjr@gmail.com>
2020-11-26 14:54:39 -0500

    refactoring code

--- Blackjack/src/com/BlackJack/Dealer.java
+++ Blackjack/src/com/BlackJack/Dealer.java
@@ -72,7 +72,7 @@ public class Dealer {
     {
         if(hand.size() == 2 && handvalue == 21)
         {
-            System.out.println("The dealer has blackjack!");
+            System.out.println("The Dealer has blackjack!");
             return true;
         }
         return false;
