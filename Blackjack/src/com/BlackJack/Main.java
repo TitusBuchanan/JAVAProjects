b5f791ee33294e40a26e1c3ff6e2748598bcaf82
titus buchanan <titusbuchananjr@gmail.com>
2020-12-07 18:26:56 -0500

    added  a method

--- Blackjack/src/com/BlackJack/Main.java
+++ Blackjack/src/com/BlackJack/Main.java
@@ -7,6 +7,18 @@ import java.util.Scanner;
 public class Main {
 
 
+        public class Blackjack {
+            private static int cash;//cash the user bets with
+            private static int bet;//how much the user wants to bet
+            private static int AceCounter;//how many aces are in the user's hand
+            private static ArrayList<Card> hand;//represents the user's hand
+            private static int handvalue;//the value of the user's hand
+            private static String name;//name of the user
+
+            public static void main(String[] args) {
+                System.out.println("Hi! What is your name?");
+                Scanner scan = new Scanner(System.in);
+            }
 
 
         }
