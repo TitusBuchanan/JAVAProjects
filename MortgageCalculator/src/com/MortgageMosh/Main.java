927b732a3847b6df395afc1e19a01c4fdf9043ef
titus buchanan <titusbuchananjr@gmail.com>
2021-01-22 20:57:30 -0500

    updated main class

--- MortgageCalculator/src/com/MortgageMosh/Main.java
+++ MortgageCalculator/src/com/MortgageMosh/Main.java
@@ -35,7 +35,7 @@ public class Main {
     private static void printPaymentSchedule(int principal, float annualInterest, byte years) {
         System.out.println();
         System.out.println("PAYMENT SCHEDULE");
-        System.out.println("-------");
+        System.out.println("------------");
         for (short month = 1; month <= years * MONTHS_IN_YEAR; month++){
             double balance = calculateBalance(principal, annualInterest, years,month);
             System.out.println(NumberFormat.getCurrencyInstance().format(balance));
